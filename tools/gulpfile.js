40caf57aba493e2c337dc23cfb70c76eda65b36e
zjj <467627364@qq.com>
2017-10-21 22:39:47 +0800

    inti preject

---
+++ tools/gulpfile.js
@@ -0,0 +1,32 @@
+//load plugins ======== create zjj ;
+const
+  gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  util = require('gulp-util'),
+  notify = require('gulp-notify'),
+  autoPrefixer = require('gulp-autoprefixer'),
+  argv = require('optimist').argv,
+  plumber = require('gulp-plumber'),
+  path = require('path');
+
+let
+  basePath = path.resolve(__dirname, '..');
+
+gulp.task('sass', cd => (
+  gulp.src(path.resolve(basePath, 'styles', '!(base)', '*.scss'))
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoPrefixer({browsers: ['last 20 versions']}))
+    .pipe(gulp.dest(path.resolve(basePath, 'dist')))
+    .pipe(notify("Found file: <%= file.relative %>!"))
+    .on("error", notify.onError(function (error) {
+      return "Message to the notifier: " + error.message;
+    }))
+));
+
+gulp.task('watch', function(){
+  gulp.watch(path.join(basePath, 'styles/**'), ['sass'])
+});
+
+
+gulp.task('default', argv.watch && ['watch']);
