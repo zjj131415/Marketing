897a47a219d3384ce9e8916ca14a82477efe76a7
zjj <467627364@qq.com>
2017-10-25 11:12:46 +0800

    close

--- html/t5/js/index.js
+++ html/t5/js/index.js
@@ -1,18 +1,48 @@
-$(function(){
-	// 页面一点击
-	$(".page1").on("click",".btn1",function(){
-		$(".page8").show();
-	});
-	$(".page1").on("click",".btn2",function(){
-		$(".page2").show();
-	});
-  $(".page1").on("click",".btn3",function(){
-    $(".page6").show();
-  });
-
-
-	// 页面二点击
-	$(".page2").on("click",".close",function(){
-		$(".page2").hide();
-	});
-});
+(function () {
+  $(function () {
+    //联动
+    var urlChina = 'js/data.js';
+    $.cxSelect.defaults.url = urlChina;
+    // 默认
+    $('#city_china').cxSelect({
+      selects: ['province', 'city', 'area']
+    });
+    $('.page1 .btn1').on('click', function () {
+      $('.page7').show();
+      $('.page1').hide();
+    });
+    $('.page1 .btn2').on('click', function () {
+      $('.page2').show();
+      $('.page1').hide();
+    });
+    $('.page1 .btn3').on('click', function () {
+      $('.page5').show();
+      $('.page1').hide();
+    });
+    $('.page2 .close').on('click', function () {
+      $('.page2').hide();
+      $('.page1').show();
+    });
+    $('.page5 .ƒorm').on('submit', function (e) {
+      e.preventDefault();
+      $('.page5').hide();
+      $('.page8').show();
+    });
+    $('.page4 .btn2').on('click', function () {
+      $('.page4').hide();
+      $('.page6').show();
+    });
+    $('.page7 .btn2').on('click', function () {
+      $('.page7').hide();
+      $('.page3').show();
+    });
+    $('.page8 .btn9').on('click', function () {
+      $('.page8').hide();
+      $('.page4').show();
+    });
+   $('.page6 .close').on('click', function () {
+     $('.page6').hide();
+     $('.page1').show();
+   });
+  })
+})()
