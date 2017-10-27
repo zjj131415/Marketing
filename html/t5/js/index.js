8ec3ec45616d8bbc631351b9f14dd997a9634da2
dianbowanghao <qwe875707281@163.com>
2017-10-27 09:16:16 +0800

    更新海报2

--- html/t5/js/index.js
+++ html/t5/js/index.js
@@ -23,71 +23,88 @@ var carSrc = "";
 
 var data = ["images/canvas_header.png", "images/footer.png"];
 document.getElementById('uploadImage').onchange = function (e) {
-  // var src, url = window.URL || window.webkitURL || window.mozURL, files = e.target.files;
-  // for (var i = 0, len = files.length; i < len; ++i) {
-  //   var file = files[i];
-  //   if (url) {
-  //     src = url.createObjectURL(file);
-  //   } else {
-  //     src = e.target.result;
-  //   }
-  // }
-  var file = e.target.files[0];
-  var Orientation = null;
-  EXIF.getData(file, function () {
-    EXIF.getAllTags(this);
-    Orientation = EXIF.getTag(this, 'Orientation');
-  });
-  var src = '';
-  var reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = function (e) {
-    var image = new Image();
-    image.src = e.target.result;
-    image.onload = function () {
-      var canvas = document.createElement("canvas");
-      canvas.width = this.naturalWidth;
-      canvas.height = this.naturalHeight;
-      var ctx = canvas.getContext("2d");
-      ctx.drawImage(this, 0, 0, this.naturalWidth, this.naturalHeight);
-      var base64 = null;
-      if (Orientation != "" && Orientation != 1 && Orientation != undefined) {
-        var width = this.naturalWidth;
-        var height = this.naturalHeight;
-        switch (Orientation) {
-          case 6://需要顺时针90度旋转
-            canvas.width = height;
-            canvas.height = width;
-            ctx.rotate(90 * Math.PI / 180);
-            ctx.drawImage(this, 0, -height);
-            break;
-          case 8://需要逆时针90度旋转
-            canvas.width = height;
-            canvas.height = width;
-            ctx.rotate(-90 * Math.PI / 180);
-            ctx.drawImage(this, -width, 0);
-            break;
-          case 3://需要180度旋转
-            ctx.rotate(180 * Math.PI / 180);
-            ctx.drawImage(this, -width, -height);
-            break;
-        }
-      }
-      src = canvas.toDataURL("image/png");
-      $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
-      $("#photo").attr("src",src);
-      $("#hecheng").attr("src",src);
-      $('.page7 .upload .uploadBg').hide();
-      $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
+  var src, url = window.URL || window.webkitURL || window.mozURL, files = e.target.files;
+  for (var i = 0, len = files.length; i < len; ++i) {
+    var file = files[i];
+    if (url) {
+      src = url.createObjectURL(file);
+    } else {
+      src = e.target.result;
+    }
+  }
 
-      var img = new Image();
-      img.src = src;
-      img.onload = function () {
-        imgEdit2 = new veImage({ canvas: document.getElementById('captureCanvas2'), image: this });
-        // $('#carImg').attr('src', imgEdit2.crop(2*pageWidth, 2*contentHeight, 0, 50));
-      };
-    };
+  src = canvas.toDataURL("image/png");
+  $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
+  $("#photo").attr("src",src);
+  $("#hecheng").attr("src",src);
+  $('.page7 .upload .uploadBg').hide();
+  $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
+
+  var img = new Image();
+  img.src = src;
+  img.onload = function () {
+    imgEdit2 = new veImage({ canvas: document.getElementById('captureCanvas2'), image: this });
+    $('#carImg').attr('src', imgEdit2.crop(2*pageWidth, 2*contentHeight, 0, 50));
   };
+
+
+
+  // var file = e.target.files[0];
+  // var Orientation = null;
+  // EXIF.getData(file, function () {
+  //   EXIF.getAllTags(this);
+  //   Orientation = EXIF.getTag(this, 'Orientation');
+  // });
+  // var src = '';
+  // var reader = new FileReader();
+  // reader.readAsDataURL(file);
+  // reader.onload = function (e) {
+  //   var image = new Image();
+  //   image.src = e.target.result;
+  //   image.onload = function () {
+  //     var canvas = document.createElement("canvas");
+  //     canvas.width = this.naturalWidth;
+  //     canvas.height = this.naturalHeight;
+  //     var ctx = canvas.getContext("2d");
+  //     ctx.drawImage(this, 0, 0, this.naturalWidth, this.naturalHeight);
+  //     var base64 = null;
+  //     if (Orientation != "" && Orientation != 1 && Orientation != undefined) {
+  //       var width = this.naturalWidth;
+  //       var height = this.naturalHeight;
+  //       switch (Orientation) {
+  //         case 6://需要顺时针90度旋转
+  //           canvas.width = height;
+  //           canvas.height = width;
+  //           ctx.rotate(90 * Math.PI / 180);
+  //           ctx.drawImage(this, 0, -height);
+  //           break;
+  //         case 8://需要逆时针90度旋转
+  //           canvas.width = height;
+  //           canvas.height = width;
+  //           ctx.rotate(-90 * Math.PI / 180);
+  //           ctx.drawImage(this, -width, 0);
+  //           break;
+  //         case 3://需要180度旋转
+  //           ctx.rotate(180 * Math.PI / 180);
+  //           ctx.drawImage(this, -width, -height);
+  //           break;
+  //       }
+  //     }
+  //     src = canvas.toDataURL("image/png");
+  //     $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
+  //     $("#photo").attr("src",src);
+  //     $("#hecheng").attr("src",src);
+  //     $('.page7 .upload .uploadBg').hide();
+  //     $('.page7 .upload .uploadShow').attr("src",src).css("display","block");
+
+  //     var img = new Image();
+  //     img.src = src;
+  //     img.onload = function () {
+  //       imgEdit2 = new veImage({ canvas: document.getElementById('captureCanvas2'), image: this });
+  //       $('#carImg').attr('src', imgEdit2.crop(2*pageWidth, 2*contentHeight, 0, 50));
+  //     };
+  //   };
+  // };
 
 
 };
