dc077327256bc7b7183f135bf07802b67d69dd7f
Your Name <you@example.com>
2017-10-31 01:16:10 +0800

    调整画布位置

--- html/t5/js/imgClip.js
+++ html/t5/js/imgClip.js
@@ -70,10 +70,10 @@ ImgClip.prototype = {
 
     // save data
     this.cutScale = parseFloat(opt.cutScale);
-    // this.winSize = { w: view().w, h: view().h-this.paddB};
+    this.winSize = { w: view().w, h: view().h-this.paddB};
     // this.winSize = { w: view().w*ratio, h: opt.cutH*ratio};
-    this.winSize = { w: view().w, h: opt.cutH};
-    // console.log(this.winSize);
+    // this.winSize = { w: view().w, h: opt.cutH};
+    console.log(this.winSize);
     opt.cutW == 'winW' ? (this.cutW = this.winSize.w) : (this.cutW = Number(opt.cutW));
     var hh = parseInt(this.cutW * this.cutScale);
     this.cutSize = {
